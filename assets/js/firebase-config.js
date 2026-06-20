cb9ed218011e0ab5798f637c26238330ab427bd3
Faraday168 <166583315+Faraday168@users.noreply.github.com>
2026-06-20 15:06:57 +0700

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBpI_ytmZvQ42bksydcD_zXg6a0F8GRtUs",
+  authDomain: "wat-nongphailom-pakchong-web.firebaseapp.com",
+  projectId: "wat-nongphailom-pakchong-web",
+  storageBucket: "wat-nongphailom-pakchong-web.firebasestorage.app",
+  messagingSenderId: "1098225420650",
+  appId: "1:1098225420650:web:81e59b09f4eda8d1f27d13",
+  measurementId: "G-H2NM4WCJWZ"
+};
